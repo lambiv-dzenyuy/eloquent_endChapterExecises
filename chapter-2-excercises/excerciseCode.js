2b744bde1d1775ec32e67aa30e415fc2b2536e26
lambiv-dzenyuy <gillslambiv@gmail.com>
2021-07-27 14:10:22 +0100

    first excercise of chapter 2

---
+++ chapter-2-excercises/excerciseCode.js
@@ -0,0 +1,32 @@
+//looping triangle
+//Write a loop that makes seven calls to console.log to output a triangle
+
+for ( var count=0; count<8; count++){
+	console.log("#".repeat(count))    
+}
+
+
+//FizzBuzz
+
+for(var count=0; count<100; count++){
+	if(count%3 == 0){
+    	console.log(`FIZZ`)}
+    else if(count%5 == 0){
+   		console.log(`BUZZ`)}
+  	else{console.log(count)}
+}
+
+//Extended version of FIZZBUZZ ABOVE
+
+for(var count=0; count<100; count++){
+    if(count%3==0 && count%5==0){
+        console,log(`FIZZBUZZ`)
+    }
+	else if(count%3 == 0){
+    	console.log(`FIZZ`)}
+    else if(count%5 == 0){
+   		console.log(`BUZZ`)}
+     
+  	else{console.log(count)}
+}
+

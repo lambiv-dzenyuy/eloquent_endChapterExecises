95f1d1718da79a120875eca7a7fff1b72db3a748
lambiv-dzenyuy <gillslambiv@gmail.com>
2021-07-27 15:15:43 +0100

    chapter 2 excercises complerion

--- chapter-2-excercises/excerciseCode.js
+++ chapter-2-excercises/excerciseCode.js
@@ -8,11 +8,12 @@ for ( var count=0; count<8; count++){
 
 //FizzBuzz
 
+
 for(var count=0; count<100; count++){
 	if(count%3 == 0){
-    	console.log(`FIZZ`)}
+    	console.log(`FIZZ`)} //PRINT FIZZ  when the number is divisile by 3
     else if(count%5 == 0){
-   		console.log(`BUZZ`)}
+   		console.log(`BUZZ`)} //print BUZZ when the number is divissible by 5
   	else{console.log(count)}
 }
 
@@ -20,7 +21,7 @@ for(var count=0; count<100; count++){
 
 for(var count=0; count<100; count++){
     if(count%3==0 && count%5==0){
-        console,log(`FIZZBUZZ`)
+        console,log(`FIZZBUZZ`) //prints FIZZBUZZ when a number is both divisible by 3 and 5
     }
 	else if(count%3 == 0){
     	console.log(`FIZZ`)}
@@ -39,3 +40,15 @@ for(var count=0; count<8; count++){
 		console.log(` #`.repeat(8))}
     else{console.log(`# `.repeat(8))}
 }
+
+//generating a general grid of given width and height sizes
+
+var width= Number(prompt("Enter the lenght of the grid"));
+var height=Number(prompt("Enter the height of the grid"));
+for(var count=0; count<height; count++){
+	if (count%2 ==0){
+		console.log(` #`.repeat(width))}
+    else{console.log(`# `.repeat(width))}
+}
+
+

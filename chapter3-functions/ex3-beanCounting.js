9086ee73fe8c8891d22b153e7d6f8385eca53ed2
lambiv-dzenyuy <gillslambiv@gmail.com>
2021-07-28 14:29:28 +0100

    completed excecises of chapter 3

--- chapter3-functions/ex3-beanCounting.js
+++ chapter3-functions/ex3-beanCounting.js
@@ -6,12 +6,35 @@ string.
 
 
 function countBs(string){
-    var numberOfBs=0;
-    const stringSize=string.lenght;
-    for(let count=0; count < stringSize; count++){
-        if(string[count]="B"){
-            numberOfBs++;
-        }
+    numberOfBs=0;
+    for(var count=0; count<string.length; count++){
+      if(string[count]=="B"){
+          numberOfBs++;
+      }
     }
     console.log(numberOfBs);
-}
+                                                 }
+    
+/*Next, write a function called countChar that behaves like countBs , except
+it takes a second argument that indicates the character that is to be counted
+(rather than counting only uppercase “B” characters). Rewrite countBs to
+make use of this new function.*/
+
+     //countChar
+
+
+     function countChar(string, ch){    //ch is the character wich is to be compared
+        numberOfBs=0;
+        for(var count=0; count<string.length; count++){
+          if(string[count]== ch){
+              numberOfBs++;
+          }
+        }
+        return numberOfBs
+                                                    }
+
+    //rewriting countBs to include countChar
+
+    function countBs(string){
+        return countChar(string, "B");
+    }

854ed398b46e23ad8c505ebd09725f96af6e7907
lambiv-dzenyuy <gillslambiv@gmail.com>
2021-07-30 15:03:19 +0100

    excercise2 completed

---
+++ chapter5_higherOrderFunctions/excercise2_myOwnLoop.js
@@ -0,0 +1,15 @@
+/*
+Write a higher-order function loop that provides something like a for loop
+statement. It takes a value, a test function, an update function, and a body
+function. Each iteration, it first runs the test function on the current loop value
+and stops if that returns false. Then it calls the body function, giving it the
+current value. Finally, it calls the update function to create a new value and
+starts from the beginning.
+*/
+
+function loop(value, testFuncion, updateFunction, bodyFunction){
+ for(let begin=value; testFuncion(begin); begin=updateFunction(begin)){
+     bodyFunction(value);
+ }
+}
+loop(4, n => n < 10, n=>n+1, console.log );

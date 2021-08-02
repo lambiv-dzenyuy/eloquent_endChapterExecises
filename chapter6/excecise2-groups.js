6d8a07e7b19ba26a9768f447fb70c00113a4c603
lambiv-dzenyuy <gillslambiv@gmail.com>
2021-08-02 17:18:19 +0100

    solved excercise 2 of chapter 6

--- chapter6/excecise2-groups.js
+++ chapter6/excecise2-groups.js
@@ -7,30 +7,47 @@ value indicating whether its argument is a member of the group.
 */
 class Group {
     // Your code here.
-    constructor(set){
+    constructor(){
         this.group=[];
+        
+    }
+  static  test(a) {
+        console.log(`hello`, a);
     }
     add(a){
-    if(!this.group.indexOf(a)){
-    this.group.push(a);
-    }
-    }
-    static from(array){
-        let generateGroup=new Group();
-        for(let count of array){ 
-            generateGroup.add(count)
+        if(!this.group.includes(a)){
+        this.group.push(a);
         }
+    }
+    has(a){
+        return this.group.includes(a);
+    }
+    delete(a){
+        let index = this.group.indexOf(a);
+        if(!(index==-1)){
+            return this.group.splice(index, 1);
+        }
+    }
+    
+    static from(a){
+       // Group.test(`god`)
+        let p= new Group();
+        for(let count of a){ 
+        p.add(count);
+        }
+      return p
+        
         
     }
     }
   
   
   let group = Group.from([10, 20]);
-  //console.log(group.has(10));
+  console.log(group.has(10));
   // → true
-  //console.log(group.has(30));
+  console.log(group.has(30));
   // → false
   group.add(10);
-  //group.delete(10);
-  //console.log(group.has(10));
+  group.delete(10);
+  console.log(group.has(10));
   // → false

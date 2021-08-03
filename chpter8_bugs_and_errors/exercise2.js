cfc9ec951ab911ecb72eeec113fee2c6b035f19a
lambiv-dzenyuy <gillslambiv@gmail.com>
2021-08-03 12:52:41 +0100

    solution to exercise 2 of chapter 8

---
+++ chpter8_bugs_and_errors/exercise2.js
@@ -0,0 +1,31 @@
+const box = {
+    locked: true,
+    unlock() { this.locked = false; },
+    lock() { this.locked = true;  },
+    _content: [],
+    get content() {
+      if (this.locked) throw new Error("Locked!");
+      return this._content;
+    }
+  };
+  
+  function withBoxUnlocked(body) {
+    // Your code here.
+    if(!(box.locked))   return body();
+  }
+  
+  withBoxUnlocked(function() {
+    box.content.push("gold piece");
+  });
+  
+  try {
+    withBoxUnlocked(function() {
+      throw new Error("Pirates on the horizon! Abort!");
+    });
+  } catch (e) {
+    console.log("Error raised: " + e);
+  }finally{
+   box.lock();
+  }
+  console.log(box.locked);
+  // → true

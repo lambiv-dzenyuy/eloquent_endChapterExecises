e42726d30c02ce8e4b26d32059f7ae2df43a27cc
unknown <lambiv-dzenyuy>
2021-08-04 13:14:32 +0100

    exercises of chapter 9

--- chapter9_regularexpressions/exercise1.js
+++ chapter9_regularexpressions/exercise1.js
@@ -8,23 +8,23 @@ verify(/pr?op/,
     ["pop culture", "mad props"],
     ["plop", "prrrop"]);
 
-verify(/ferr[earity]/,
+verify(/ferr(et|y|ari)/,
     ["ferret", "ferry", "ferrari"],
     ["ferrum", "transfer A"]);
 
-verify(/^\w+.*(ious)\b$/y,
+verify(/ious\b/,
     ["how delicious", "spacious room"],
     ["ruinous", "consciousness"]);
 
-verify(/\s(.|,)/,
+verify(/\s[.,:;]/,
     ["bad punctuation ."],
     ["escape the period"]);
 
-verify(/.../,
+verify(/\w{7}/,
     ["Siebentausenddreihundertzweiundzwanzig"],
     ["no", "three small words"]);
 
-verify(/.../,
+verify(/\b[^\We]+\b/,
     ["red platypus", "wobbling nest"],
     ["earth bed", "learning ape", "BEET"]);
 
